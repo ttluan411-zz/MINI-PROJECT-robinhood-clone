3205b8cf4e8b709a9145f4af6eaffbcc4d31d5b4
Logan Truong <ttluan411@gmail.com>
2017-10-27 14:51:50 -0700

    collapsed nav added

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,27 +1,47 @@
 import React, {Component} from 'react';
 import '../Style/Nav.css';
+import classNames from 'classnames';
 
 export default class Nav extends Component {
-  constructor(props){
-    super(props);
+  constructor(){
+    super();
     this.state = {
-
+      scrolled: false
     }
+    this.handleScroll = this.handleScroll.bind(this);
   }
+
+  componentDidMount(){
+    window.addEventListener('scroll', this.handleScroll)
+  }
+  handleScroll(event) {
+    console.log(event.srcElement.scrollingElement.scrollTop, this.state)
+    	if(event.srcElement.scrollingElement.scrollTop > 200) {
+    		this.setState({
+    			scrolled: true
+    		})
+    	} else {
+    		this.setState({
+    			scrolled: false
+    	   })
+      }
+    }
 
   render() {
     return (
       <div className="nav">
+        <div className={this.state.scrolled ? "nav-scrolled" : "nav"}>
         <div className="nav-container">
           <div className="nav-logo">
           </div>
           <div className="nav-buttons-container">
-            <div className="career-button">CAREERS</div>
-            <div className="help-button">HELP</div>
-            <div className="blog-button">BLOG</div>
+            <div className="route-button">CAREERS</div>
+            <div className="route-button">HELP</div>
+            <div className="route-button">BLOG</div>
             <div className="signup-button">SIGN UP</div>
           </div>
         </div>
+      </div>
       </div>
     )
   }
